1d45abd9b20ad54fd72cdc9d87d843e37a98ed80
virendra uikey <91123737+Virendrauikey@users.noreply.github.com>
2022-08-01 02:28:00 +0530

    login register

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import bodyParser from 'body-parser';
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 mongoose.connect("mongodb://localhost:27017/myLoginRegisterDB", {
